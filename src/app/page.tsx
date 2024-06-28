414c6edfc880a639953f6181555d9077ac7d70bf
lathiyaharsh <patoliya.hl1@gmail.com>
2024-06-28 15:32:59 +0530

    fix errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,8 @@ import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
-  const token = localStorage.getItem("token");
-  if (token) {
-    router.push("/dashboard");
-  } else {
-    router.push("/login");
-  }
+
+  router.push("/dashboard");
+
   return <></>;
 }
--- src/service/getUserDetails.tsx
+++ src/service/getUserDetails.tsx
@@ -1,3 +1,4 @@
+"use client"
 import axios from "axios";
 
 const baseUrl = process.env.NEXT_PUBLIC_BASEURL;
