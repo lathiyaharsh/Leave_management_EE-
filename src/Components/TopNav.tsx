9c707f73f97eea962ce2da0f87d43b2f60a7f762
lathiyaharsh <patoliya.hl1@gmail.com>
2024-07-01 09:52:57 +0530

    update topnav

--- src/Components/TopNav.tsx
+++ src/Components/TopNav.tsx
@@ -15,7 +15,7 @@ const TopNavBar = () => {
 
   return (
     <>
-      <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+      <nav className="fixed top-0 z-50 w-full border-b border-gray-200 bg-gray-800 dark:border-gray-700">
         <div className="px-3 py-3 lg:px-5 lg:pl-3">
           <div className="flex items-center justify-between">
             <div className="flex items-center justify-start rtl:justify-end">
@@ -33,7 +33,7 @@ const TopNavBar = () => {
                 />
               </button>
               <Link href="/dashboard" className="flex ms-2 md:me-24">
-              <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
+              <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap text-white">
                   Lms
                 </span>
               </Link>
